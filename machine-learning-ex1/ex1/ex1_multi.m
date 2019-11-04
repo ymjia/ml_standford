8a95a9178e215fd81d2729fc383528e324d989cd
ymjia <ymjia90@163.com>
2019-11-04 19:18:42 +0800

    ex1: use gd in single feature for multi-feature, use large learning rate, finish normal equation

--- machine-learning-ex1/ex1/ex1_multi.m
+++ machine-learning-ex1/ex1/ex1_multi.m
@@ -44,7 +44,7 @@ fprintf('First 10 examples from the dataset: \n');
 fprintf(' x = [%.0f %.0f], y = %.0f \n', [X(1:10,:) y(1:10,:)]');
 
 fprintf('Program paused. Press enter to continue.\n');
-kbhit;
+%kbhit;
 
 % Scale features and set them to zero mean
 fprintf('Normalizing Features ...\n');
@@ -80,18 +80,27 @@ X = [ones(m, 1) X];
 %
 
 fprintf('Running gradient descent ...\n');
-kbhit;
+
 % Choose some alpha value
-alpha = 0.01;
+alpha = 0.1;
 num_iters = 400;
 
 % Init Theta and Run Gradient Descent 
 theta = zeros(3, 1);
 [theta, J_history] = gradientDescentMulti(X, y, theta, alpha, num_iters);
-
+[theta2, J_history2] = gradientDescentMulti(X, y, theta, 1.5, 100);
+[theta3, J_history3] = gradientDescentMulti(X, y, theta, 0.1, num_iters);
+[theta4, J_history4] = gradientDescentMulti(X, y, theta, 0.3, num_iters);
 % Plot the convergence graph
 figure;
+subplot(2, 2, 1)
 plot(1:numel(J_history), J_history, '-b', 'LineWidth', 2);
+subplot(2, 2, 2)
+plot(1:numel(J_history2), J_history2, '-r', 'LineWidth', 2);
+subplot(2, 2, 3)
+plot(1:numel(J_history3), J_history3, '-g', 'LineWidth', 2);
+subplot(2, 2, 4)
+plot(1:numel(J_history4), J_history4, '-k', 'LineWidth', 2);
 xlabel('Number of iterations');
 ylabel('Cost J');
 
@@ -104,16 +113,15 @@ fprintf('\n');
 % ====================== YOUR CODE HERE ======================
 % Recall that the first column of X is all-ones. Thus, it does
 % not need to be normalized.
-price = 0; % You should change this
+price = theta' * [1 ([1650 3] .- mu) ./sigma]'; % You should change this
 
 
 % ============================================================
 
 fprintf(['Predicted price of a 1650 sq-ft, 3 br house ' ...
          '(using gradient descent):\n $%f\n'], price);
-
 fprintf('Program kbhitd. Press enter to continue.\n');
-kbhit;
+
 
 %% ================ Part 3: Normal Equations ================
 
@@ -149,8 +157,7 @@ fprintf('\n');
 
 % Estimate the price of a 1650 sq-ft, 3 br house
 % ====================== YOUR CODE HERE ======================
-price = 0; % You should change this
-
+price = theta' * [1 1650 3]'; % You should change this
 
 % ============================================================
 
